940ecc12dde256478ea55713fdf7bcc58d6e0f80
Harrison Walters <hwalters@uvic.ca>
2017-03-09 12:26:55 -0800

    framework

--- SVM.py
+++ SVM.py
@@ -4,10 +4,14 @@
 def TrainOnData():
 	#In this mode, any text files "data1", "data2", "data3", etc, are used to train the SVM.
 	# TO DO 
-	print("train")
+	print("Training SVM on collected data...")
+	print("Complete.")
 	
 def TestOnData():
 	#In this mode, the previously trained SVM should attempt to classify data files "test1", "test2", "test3", etc.. using the trained SVM
 	# TO DO
-	print("test")
+	print("Running SVM on test data...")
+	accuracy = 0.0
+	print("Complete.")
+	print("accuracy = "+str(accuracy))
 
--- Scraper.py
+++ Scraper.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+
 def StartCollectingData():	
 	#In this mode, you leave your computer to collect data (possibly over several days)
 	#Note : data should be stored in files "data1", "data2", "data3", etc ... should a crash occur, not all data is lost
 	#TO DO
-	print("mine")
-
-
-
+	print("Starting in data-mine mode [press Ctrl+C to stop]...")
+	samples_collected = 0
+	time_elapsed = 0
+	while(True):
+		time_elapsed = time_elapsed+1
+		print("time elapsed : "+str(time_elapsed)+". samples collected : "+str(samples_collected))
+		time.sleep(1.0)
