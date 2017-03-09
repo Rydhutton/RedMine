342f1bd64233c38b82600f99ab010776fbe1c75d
Harrison Walters <hwalters@uvic.ca>
2017-03-09 12:19:59 -0800

    framework

--- SVM.py
+++ SVM.py
@@ -3,9 +3,11 @@
 
 def TrainOnData():
 	#In this mode, any text files "data1", "data2", "data3", etc, are used to train the SVM.
+	# TO DO 
 	print("train")
 	
 def TestOnData():
 	#In this mode, the previously trained SVM should attempt to classify data files "test1", "test2", "test3", etc.. using the trained SVM
+	# TO DO
 	print("test")
 
--- Scraper.py
+++ Scraper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def StartCollectingData():
+def StartCollectingData():	
 	#In this mode, you leave your computer to collect data (possibly over several days)
-	#Note : data should be stored in files "data1", "data2", "data3", etc ... so that if a crash occurs, not all data is lost
+	#Note : data should be stored in files "data1", "data2", "data3", etc ... should a crash occur, not all data is lost
+	#TO DO
 	print("mine")
 
 
