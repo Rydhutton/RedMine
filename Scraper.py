a66a3a2e020605324d1d385219947ad2c78b22b1
Harrison Walters <hwalters@uvic.ca>
2017-03-09 12:30:02 -0800

    framework

--- Scraper.py
+++ Scraper.py
@@ -8,9 +8,10 @@ def StartCollectingData():
 	#Note : data should be stored in files "data1", "data2", "data3", etc ... should a crash occur, not all data is lost
 	#TO-DO !!!
 	print("Starting in data-mine mode [press Ctrl+C to stop]...")
-	samples_collected = 0
+	incomplete_samples = 0
+	complete_samples = 0
 	time_elapsed = 0
 	while(True):
 		time_elapsed = time_elapsed+1
-		print("time elapsed : "+str(time_elapsed)+". samples collected : "+str(samples_collected))
+		print("time elapsed : "+str(time_elapsed)+". incomplete samples : "+str(incomplete_samples)+". complete samples : "+str(complete_samples))
 		time.sleep(1.0)
