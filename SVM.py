e4b6371e9c5dd96639343867b85c7400ce662962
Harrison Walters <hwalters@uvic.ca>
2017-03-09 15:05:45 -0800

    framework

--- SVM.py
+++ SVM.py
@@ -4,14 +4,23 @@
 def TrainOnData():
 	#In this mode, any text files "data1", "data2", "data3", etc, are used to train the SVM.
 	#TO-DO !!!
-	print("Training SVM on collected data...")
+	print("Training SVM on collected data.")
+	n_files_detected = 0
+	n_samples = 0
+	print("...found "+str(n_files_detected)+" data files for training")
+	print("...loaded "+str(n_samples)+" data samples")
 	print("Complete.")
 	
 def TestOnData():
 	#In this mode, the previously trained SVM should attempt to classify data files "test1", "test2", "test3", etc.. using the trained SVM
 	#TO-DO !!!
-	print("Running SVM on test data...")
+	print("Running SVM on test data.")
 	accuracy = 0.0
+	n_files_detected = 0
+	n_samples = 0
+	print("...found "+str(n_files_detected)+" data files for testing")
+	print("...loaded "+str(n_samples)+" data samples")
+	print("...SVM accuracy = "+str(accuracy))
 	print("Complete.")
-	print("accuracy = "+str(accuracy))
+
 
--- Scraper.py
+++ Scraper.py
@@ -7,11 +7,11 @@ def StartCollectingData():
 	#In this mode, you leave your computer to collect data (possibly over several days)
 	#Note : data should be stored in files "data1", "data2", "data3", etc ... should a crash occur, not all data is lost
 	#TO-DO !!!
-	print("Starting in data-mine mode [press Ctrl+C to stop]...")
+	print("Starting in data-mine mode [press Ctrl+C to stop].")
 	incomplete_samples = 0 # since samples require data like '10 min', '20 min', '2 hours', some samples will be marked 'incomplete'
 	complete_samples = 0
 	time_elapsed = 0
 	while(True):
 		time_elapsed = time_elapsed+1
-		print("time elapsed : "+str(time_elapsed)+". incomplete samples : "+str(incomplete_samples)+". complete samples : "+str(complete_samples))
+		print("...time elapsed : "+str(time_elapsed)+". incomplete samples : "+str(incomplete_samples)+". complete samples : "+str(complete_samples))
 		time.sleep(1.0)
