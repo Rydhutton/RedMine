f2423d7cc4e5abf257e7652b9a57ed9e485d91d7
Harrison Walters <hwalters@uvic.ca>
2017-03-12 00:05:20 -0800

    update

--- Scraper.py
+++ Scraper.py
@@ -40,7 +40,7 @@ def StartCollectingData():
 		
 def LogNewSubmissions():
 
-	# [harrison] initialize session
+	# [harrison] initialize session with PRAW
 	reddit = praw.Reddit(client_id='Er23cgYvVuqPHw', client_secret='uXfAKsBIUQ7JaR6Hy--RxQuF4eo', user_agent='CompSci474Project:v1.0.0 (by /u/csc475_user)')
 	subreddits_to_monitor = ['AskReddit', 'funny', 'todayilearned', 'science', 'worldnews', 'pics', 'IAmA', 'gaming', 'videos', 'movies', 'Music', 'aww', 'news', 'gifs', 'explainlikeimfive', 'askscience', 'EarthPorn', 'books', 'television', 'LifeProTips', 'mildlyinteresting', 'DIY', 'Showerthoughts', 'space', 'sports', 'InternetIsBeautiful', 'tifu', 'Jokes', 'history', 'gadgets', 'food', 'nottheonion', 'photoshopbattles', 'Futurology', 'Documentaries', 'personalfinance', 'dataisbeautiful', 'GetMotivated', 'UpliftingNews', 'listentothis']
 	cct = ''
