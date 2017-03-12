4476e7c3d69e1b5c9b0884974a1b1fc95c1a5111
Harrison Walters <hwalters@uvic.ca>
2017-03-11 16:23:55 -0800

    update

--- Scraper.py
+++ Scraper.py
@@ -6,15 +6,23 @@ import praw
 
 # we are using "PRAW" as an API to collect reddit data
 # you can install it simply by typing "pip install praw" into the terminal
-# you can see the app details if you
+# you can see the app details (client_id/secret/etc) if you
 #	1) log in to reddit as csc475_user (pass:asdfasdf)
 #	2) go to 'preferences'
 #	3) click on the 'apps' tab
 
 def StartCollectingData():	
-	print("Starting in data-mine mode [press Ctrl+C to stop].")
 	
 	reddit = praw.Reddit(client_id='Er23cgYvVuqPHw', client_secret='uXfAKsBIUQ7JaR6Hy--RxQuF4eo', user_agent='CompSci474Project:v1.0.0 (by /u/csc475_user)')
+	
+	# these are the 40 most popular subreddits
+	subreddits_to_monitor = ['AskReddit', 'funny', 'todayilearned', 'science', 'worldnews', 'pics', 'IAmA', 'gaming', 'videos', 'movies', 'Music', 'aww', 'news', 'gifs', 'explainlikeimfive', 'askscience', 'EarthPorn', 'books', 'television', 'LifeProTips', 'mildlyinteresting', 'DIY', 'Showerthoughts', 'space', 'sports', 'InternetIsBeautiful', 'tifu', 'Jokes', 'history', 'gadgets', 'food', 'nottheonion', 'photoshopbattles', 'Futurology', 'Documentaries', 'personalfinance', 'dataisbeautiful', 'GetMotivated', 'UpliftingNews', 'listentothis']
+	
+	print("Starting in data-mine mode [press Ctrl+C to stop].")
+	
+	
+	
+	
 	
 	# harrison is working on this as we speak
 	# please do not touch lol
