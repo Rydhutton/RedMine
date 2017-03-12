10eab7727a15f0d732089f2388b267f57088a1de
Harrison Walters <hwalters@uvic.ca>
2017-03-11 23:20:24 -0800

    update

--- Scraper.py
+++ Scraper.py
@@ -22,33 +22,30 @@ def StartCollectingData():
 	
 	# [harrison] initialize session
 	reddit = praw.Reddit(client_id='Er23cgYvVuqPHw', client_secret='uXfAKsBIUQ7JaR6Hy--RxQuF4eo', user_agent='CompSci474Project:v1.0.0 (by /u/csc475_user)')
-	subreddits_to_monitor = ['AskReddit', 'funny', 'todayilearned', 'science', 'worldnews', 'pics', 'IAmA', 'gaming', 'videos', 'movies', 'Music', 'aww', 'news', 'gifs', 'explainlikeimfive', 'askscience', 'EarthPorn', 'books', 'television', 'LifeProTips', 'mildlyinteresting', 'DIY', 'Showerthoughts', 'space', 'sports', 'InternetIsBeautiful', 'tifu', 'Jokes', 'history', 'gadgets', 'food', 'nottheonion', 'photoshopbattles', 'Futurology', 'Documentaries', 'personalfinance', 'dataisbeautiful', 'GetMotivated', 'UpliftingNews', 'listentothis']
-	cct = ''
-	for i in range(len(subreddits_to_monitor)):
-		cct = cct+subreddits_to_monitor[i]
-		if (i != len(subreddits_to_monitor)-1):
-			cct = cct+'+'
-	to_stream = reddit.subreddit(cct)
-	
-	reeval = 60.0 * 15 # re-evalutate each post after 15 minutes
+	#subreddits_to_monitor = ['AskReddit', 'funny', 'todayilearned', 'science', 'worldnews', 'pics', 'IAmA', 'gaming', 'videos', 'movies', 'Music', 'aww', 'news', 'gifs', 'explainlikeimfive', 'askscience', 'EarthPorn', 'books', 'television', 'LifeProTips', 'mildlyinteresting', 'DIY', 'Showerthoughts', 'space', 'sports', 'InternetIsBeautiful', 'tifu', 'Jokes', 'history', 'gadgets', 'food', 'nottheonion', 'photoshopbattles', 'Futurology', 'Documentaries', 'personalfinance', 'dataisbeautiful', 'GetMotivated', 'UpliftingNews', 'listentothis']
+	#n_subscribers_to_subreddit = { }
+	#cct = ''
+	#for i in range(len(subreddits_to_monitor)):
+	#	cct = cct+subreddits_to_monitor[i]
+	#	if (i != len(subreddits_to_monitor)-1):
+	#		cct = cct+'+'
+	to_stream = reddit.subreddit('all')
 	
 	for P in to_stream.stream.submissions():
-		
+	
 		# [harrison] create new instance for new post, put in incomplete_queue
+		u = reddit.redditor(str(P.author)) # user
+		CK = u.comment_karma
+		LK = u.link_karma
 		d = { }
-		d['ts'] = time.time()
-		d['pid'] = P.id
+		d['pid'] = P.id #book-keeping
+		d['ts'] = time.time() #book-keeping & PARAM
+		d['CK'] = CK #PARAM
+		d['LK'] = LK #PARAM
+		#print("subreddit: "+str(P.subreddit))
 		queue_incomplete.append(d)
-		
-		# [harrison] check if old data needs to be updated
-		if (len(queue_incomplete) != 0):
-			while(queue_incomplete[0]['ts']+reeval < time.time()):
-				queue_complete.append( queue_incomplete[0] )
-				del queue_incomplete[0]
-				if (len(queue_incomplete) == 0): break
-		
+				
 		# [harrison] debug
-		print('\n'+str((P.title).encode('utf-8'))+' [id='+P.id+']')
+		#print('\n'+str((P.title).encode('utf-8'))+' [id='+P.id+']')
 		print(P.subreddit)
-		print(time.ctime(time.time()))
-		print('queue lenghts = ['+str(len(queue_incomplete))+','+str(len(queue_complete))+']\n')
+		#print('queues = ['+str(len(queue_incomplete))+','+str(len(queue_complete))+']\n')
