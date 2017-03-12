788a5570313443aa47efc7e1b7f9830694d88aa9
Harrison Walters <hwalters@uvic.ca>
2017-03-11 18:30:53 -0800

    update

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import threading
 import time
 from time import gmtime, strftime
 import praw
@@ -12,12 +13,13 @@ import praw
 #	2) go to 'preferences'
 #	3) click on the 'apps' tab
 
+queue_incomplete = []
+queue_complete = []
+
 def StartCollectingData():	
-	
-	# harrison is working on this as we speak
-	# please do not touch lol
-	
-	print("Starting in data-mine mode [press Ctrl+C to stop].")
+
+	print("\nStarting in data-mine mode [press Ctrl+C to stop].")
+		
 	reddit = praw.Reddit(client_id='Er23cgYvVuqPHw', client_secret='uXfAKsBIUQ7JaR6Hy--RxQuF4eo', user_agent='CompSci474Project:v1.0.0 (by /u/csc475_user)')
 	subreddits_to_monitor = ['AskReddit', 'funny', 'todayilearned', 'science', 'worldnews', 'pics', 'IAmA', 'gaming', 'videos', 'movies', 'Music', 'aww', 'news', 'gifs', 'explainlikeimfive', 'askscience', 'EarthPorn', 'books', 'television', 'LifeProTips', 'mildlyinteresting', 'DIY', 'Showerthoughts', 'space', 'sports', 'InternetIsBeautiful', 'tifu', 'Jokes', 'history', 'gadgets', 'food', 'nottheonion', 'photoshopbattles', 'Futurology', 'Documentaries', 'personalfinance', 'dataisbeautiful', 'GetMotivated', 'UpliftingNews', 'listentothis']
 	all_subreddits = ''
@@ -26,21 +28,21 @@ def StartCollectingData():
 		if (i != len(subreddits_to_monitor)-1):
 			all_subreddits = all_subreddits+'+'
 	to_stream = reddit.subreddit(all_subreddits)
-	
-	queue_incomplete = []
-	queue_complete = []
-	
+		
 	for P in to_stream.stream.submissions():
 		
 		# create new instance for new post, put in incomplete_queue
-		dict = { }
+		d = { }
 		T = time.time()
-		dict['timestamp'] = T
-		queue_incomplete.append(dict)
+		d['timestamp'] = T
+		d['pid'] = P.id
+		queue_incomplete.append(d)
+		
+		# check if old data needs to be updated
+		
 		
 		# print [debug]
-		title = (P.title).encode('utf-8')
-		print('\n'+str(title))
+		print('\n'+str((P.title).encode('utf-8'))+' [id='+P.id+']')
 		print(P.subreddit)
 		print(time.ctime(T))
 		print('queue lenghts = ['+str(len(queue_incomplete))+','+str(len(queue_complete))+']\n')
